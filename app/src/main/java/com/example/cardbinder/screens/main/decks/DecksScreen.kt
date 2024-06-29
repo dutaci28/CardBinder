62c72be8e24bf71ef5aa3b7a7d6509d2676f1594
dutaci28 <dutaci28@gmail.com>
2024-06-30 02:58:43 +0300

    added wip marker to decks screen

--- app/src/main/java/com/example/cardbinder/screens/main/decks/DecksScreen.kt
+++ app/src/main/java/com/example/cardbinder/screens/main/decks/DecksScreen.kt
@@ -1,8 +1,14 @@
 package com.example.cardbinder.screens.main.decks
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.cardbinder.screens.navigation.NavigationRoutes
@@ -15,13 +21,22 @@ fun DecksScreen(
     navController: NavController
 ) {
     val auth = Firebase.auth
-    Button(onClick = {
-        auth.signOut()
-        navController.navigate(route = NavigationRoutes.LogIn.route) {
-            popUpTo(route = NavigationRoutes.LogIn.route)
-            launchSingleTop = true
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "WIP",
+                color = Color.Red
+            )
+            Button(onClick = {
+                auth.signOut()
+                navController.navigate(route = NavigationRoutes.LogIn.route) {
+                    popUpTo(route = NavigationRoutes.LogIn.route)
+                    launchSingleTop = true
+                }
+            }) {
+                Text(text = "Log Out")
+            }
         }
-    }) {
-        Text(text = "Log Out")
+
     }
 }
